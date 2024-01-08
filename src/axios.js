7d7def75881b6ad202682d8964d8bd86335bee58
MatveyPetrov1 <matveygemot@gmail.com>
2024-01-08 17:44:21 +0700

    second commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4444",
+  baseURL: "vlasovproductionbackend-production.up.railway.app",
 });
 
 export default instance;
